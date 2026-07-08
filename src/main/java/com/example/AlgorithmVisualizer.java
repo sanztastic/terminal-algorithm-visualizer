982dfb6b85849b3fbc12fa3bac10c8b3e9e10526
shanand <shanandshrestha@gmail.com>
2026-07-08 21:14:03 +0545

    removing swing code as not using swing using terminal

--- src/main/java/com/example/AlgorithmVisualizer.java
+++ src/main/java/com/example/AlgorithmVisualizer.java
@@ -1,17 +1,7 @@
 package com.example;
 
-import java.awt.Toolkit;
-import javax.swing.JFrame;
-
-public class AlgorithmVisualizer extends JFrame {
+public class AlgorithmVisualizer {
     public void drawScreen(){
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = kit.getScreenSize();
-        int screenHeight = screenSize.height;
-        int screenWidth = screenSize.width; 
         
-        this.setSize(screenWidth, screenHeight);
-
-
     }
 }
